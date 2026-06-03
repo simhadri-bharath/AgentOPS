26f8dcafa8d29869a05b0f6393e5e9b7a374c996
nihith-covasant <simhadribharath2004@gmail.com>
2026-06-03 09:43:06 +0530

    feat(evaluation_runs): Add name and updated_at columns to evaluation_runs table

--- backend/alembic/versions/004_evaluation_job_fields.py
+++ backend/alembic/versions/004_evaluation_job_fields.py
@@ -1,7 +1,7 @@
 """Add name and updated_at to evaluation_runs (evaluation jobs)
 
-Revision ID: 003
-Revises: 002
+Revision ID: 004
+Revises: 003
 Create Date: 2026-06-01
 
 """
@@ -11,8 +11,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "003"
-down_revision: Union[str, None] = "002"
+revision: str = "004"
+down_revision: Union[str, None] = "003"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
